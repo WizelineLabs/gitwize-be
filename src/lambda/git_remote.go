0831384e811f3088900dc5f83c9c51c27d0bfb6f
Sang Dinh <sang.dinh@wizeline.com>
2020-05-08 00:42:19 +0700

    GWZ-37 adding db connection, read remote branch, go mod

---
+++ src/lambda/git_remote.go
@@ -0,0 +1,36 @@
+package lambda
+
+import (
+	"github.com/go-git/go-git/v5"
+	"github.com/go-git/go-git/v5/config"
+	"github.com/go-git/go-git/v5/plumbing"
+	"github.com/go-git/go-git/v5/storage/memory"
+	"strings"
+)
+
+type remoteClient interface {
+	List(options *git.ListOptions) ([]*plumbing.Reference, error)
+}
+
+func GetRemoteBranches(client remoteClient) []string {
+	refList, err := client.List(&git.ListOptions{})
+	if err != nil {
+		panic(err)
+	}
+	refPrefix := "refs/heads/"
+	result := []string{}
+	for _, ref := range refList {
+		refName := ref.Name().String()
+		if !strings.HasPrefix(refName, refPrefix) {
+			continue
+		}
+		branchName := refName[len(refPrefix):]
+		result = append(result, branchName)
+	}
+	return result
+}
+
+func GetRemoteBranchesNoAuth(url string) []string {
+	remote := git.NewRemote(memory.NewStorage(), &config.RemoteConfig{Name: "origin", URLs: []string{url}})
+	return GetRemoteBranches(remote)
+}
