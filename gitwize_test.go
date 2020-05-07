4837f5b2b43c4eca8cb9589bf86fa68a166504f0
Cuong Le <cuong.le@wizeline.com>
2020-05-07 17:56:45 +0700

    GWZ-23 verifies access token
    add auth module + unit tests

--- gitwize_test.go
+++ gitwize_test.go
@@ -1,9 +1,44 @@
 package main
 
-import "testing"
+import (
+	"testing"
+	"net/http"
+	"gitwize-be/src/auth"
+)
 
-func TestPass(t *testing.T) {}
+// --- auth.go tests ---
+type mockJWTVerifier struct{}
 
-func TestFail(t *testing.T) {
-	// t.Error("This is an error testcase example")
+func (m mockJWTVerifier) Verify(token string) bool {
+	if token == "valid-token" {
+		return true
+	}
+	return false
 }
+
+func Test_Request_Authorized(t *testing.T) {
+	r := &http.Request {
+		Header: map[string][]string{
+					"Authorization": {"Bearer valid-token"},
+		},
+	}
+	v := mockJWTVerifier{}
+	authorized := auth.IsAuthorized(v, r)
+	if !authorized {
+		t.Error("Expected authorized!")
+	}
+}
+
+func Test_Request_UnAuthorized(t *testing.T) {
+	r := &http.Request {
+		Header: map[string][]string{
+					"Authorization": {"Bearer XXXXXX"},
+		},
+	}
+	v := mockJWTVerifier{}
+	authorized := auth.IsAuthorized(v, r)
+	if authorized {
+		t.Error("Expected unauthorized!")
+	}
+}
+// --- end auth.go tests ---
