e5ab7a56cc8668e85306a1860c8b35094c919d23
Cuong Le <cuong.le@wizeline.com>
2020-05-26 16:09:26 +0700

    GWZ-41 collect PRs for all repos in repository table

--- src/lambda/pull_request.go
+++ src/lambda/pull_request.go
@@ -4,7 +4,9 @@ import (
 	"context"
 	"database/sql"
 	"gitwize-be/src/db"
+	"log"
 	"os"
+	"strings"
 
 	"github.com/google/go-github/v31/github"
 	"golang.org/x/oauth2"
@@ -39,13 +41,47 @@ func (s *GithubPullRequestService) List(owner string, repo string, opts *github.
 	return s.githubClient.PullRequests.List(context.Background(), owner, repo, opts)
 }
 
-func CollectPRs(prSvc PullRequestService, owner string, repo string) {
+func CollectPRs(prSvc PullRequestService) {
+	// find repo id
+	conn := db.SqlDBConn()
+	rows, _ := conn.Query("SELECT url FROM repository")
+
+	var url string
+	for rows.Next() {
+		err := rows.Scan(&url)
+		if err != nil {
+			log.Printf("[ERROR] %s", err)
+		} else {
+			CollectPRsFromUrl(prSvc, url)
+		}
+	}
+}
+
+func CollectPRsFromUrl(prSvc PullRequestService, url string) {
+	var owner, repo string
+	if strings.HasPrefix(url, "git") {
+		s := strings.Split(url, ":")[1]
+		owner = strings.Split(s, "/")[0]
+		repo = strings.Split(s, "/")[1]
+	}
+	if strings.HasPrefix(url, "https") {
+		s := strings.Split(url, "/")
+		owner = s[len(s)-2]
+		repo = s[len(s)-1]
+	}
+	repo = strings.Replace(repo, ".git", "", -1)
+	log.Printf("Collecting PRs: owner=%s, repo=%s", owner, repo)
+	collectPRs(prSvc, owner, repo)
+}
+
+func collectPRs(prSvc PullRequestService, owner string, repo string) {
 	prs, _, err := prSvc.List(owner, repo, &github.PullRequestListOptions{
 		State: "all",
 	})
 
 	if err != nil {
-		panic(err)
+		log.Printf("[ERROR] %s", err)
+		return
 	}
 
 	// find repo id
@@ -55,7 +91,8 @@ func CollectPRs(prSvc PullRequestService, owner string, repo string) {
 	err = rows.Scan(&repoID)
 
 	if err != nil {
-		panic(err)
+		log.Printf("[ERROR] %s", err)
+		return
 	}
 
 	updatePRs(prSvc, repoID, prs, conn)
@@ -66,7 +103,8 @@ func updatePRs(prSvc PullRequestService, repoID int, prs []*github.PullRequest, 
 	insertStmt, err := conn.Prepare("INSERT INTO pull_request (repository_id, url, pr_no, title, body, head, base, state, created_by, created_year, created_month, created_day, created_hour, closed_year, closed_month, closed_day, closed_hour) VALUES(?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?)")
 	deleteStmt, err := conn.Prepare("DELETE FROM pull_request WHERE repository_id = ?")
 	if err != nil {
-		panic(err.Error())
+		log.Printf("[ERROR] %s", err)
+		return
 	}
 	defer insertStmt.Close()
 	defer deleteStmt.Close()
@@ -74,7 +112,8 @@ func updatePRs(prSvc PullRequestService, repoID int, prs []*github.PullRequest, 
 	// delete old data
 	_, err = deleteStmt.Exec(repoID)
 	if err != nil {
-		panic(err)
+		log.Printf("[ERROR] %s", err)
+		return
 	}
 
 	// insert again
