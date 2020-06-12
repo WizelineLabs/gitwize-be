12890958012b3383c7fec593af2094b43a69aad0
raymondgao <raymond.gao@wizeline.com>
2020-06-12 10:12:48 +0700

    Deploy qa (#34)
    * setup CD for QA

--- src/configuration/common.go
+++ src/configuration/common.go
@@ -3,13 +3,17 @@ package configuration
 const (
 	gwDeployEnv              = "GW_DEPLOY_ENV"
 	gwDbPasswordDev          = "GW_DATABASE_SECRET_DEV"
+	gwDbPasswordQA           = "GW_DATABASE_SECRET_QA"
 	gwDbPasswordLocal        = "GW_DATABASE_SECRET_LOCAL"
 	devEnvironment           = "DEV"
+	qaEnvironment            = "QA"
 	configDev                = "config_dev"
+	configQA                 = "config_qa"
 	configLocal              = "config_local"
 	configTypeYaml           = "yaml"
 	configPathFromRootDir    = "src/configuration"
 	configPathFromSubModules = "../configuration"
 	cypherPassPhaseLocal     = "CYPHER_PASS_PHASE_LOCAL"
 	cypherPassPhaseDev       = "CYPHER_PASS_PHASE_DEV"
+	cypherPassPhaseQA        = "CYPHER_PASS_PHASE_QA"
 )
