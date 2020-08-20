d653b3002f636ae1edec0f36c76ca679d530475a
Khoa Nguyen <khoa.nguyen@wizeline.com>
2020-08-20 13:04:51 +0700

    Trivial: Deduplicate unusual files stats

--- src/db/unusual_files.go
+++ src/db/unusual_files.go
@@ -10,7 +10,8 @@ func GetUnusualFiles(repID string, since, until time.Time) ([]FileDetail, error)
 	files := make([]FileDetail, 0)
 	err := gormDB.
 		Where("repository_id = ? AND commit_time_stamp >= ? AND commit_time_stamp <= ? AND addition_loc > 1000", repID, since, until).
-		Select("file_name, addition_loc").Find(&files).Error
+		Select("file_name, sum(addition_loc) as addition_loc, sum(deletion_loc) as deletion_loc").
+		Group("file_name").Find(&files).Error
 	if err != nil {
 		if !gorm.IsRecordNotFoundError(err) {
 			return nil, err
